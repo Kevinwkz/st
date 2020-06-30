1d92c8f033f4ce5ce07ac4489afd4a9478337a75
Kevinwkz <kevinntavares@gmail.com>
2020-06-29 22:54:31 -0300

    added emoji support

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Noto Color Emoji:style=Regular:pixelsize=16:antialias=true:autohint=true";
+static char *font = "mono:style=Bold:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 static int borderpx = 8;
 
 /*
@@ -175,6 +176,7 @@ static uint forcemousemod = ShiftMask;
  */
 ResourcePref resources[] = {
 		{ "font",         STRING,  &font },
+        { "fontalt0",     STRING,  &font2[0] },
 		{ "color0",       STRING,  &colorname[0] },
 		{ "color1",       STRING,  &colorname[1] },
 		{ "color2",       STRING,  &colorname[2] },
